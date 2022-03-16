0d00ed836d4d095bba3098b899ea9229970bca67
IgorALopes <igor9487@gmail.com>
2022-03-16 14:08:56 -0300

    Add mais certificados Dio

---
+++ Dio-Variáveis,_tipos_de_dados_e_operadores_maetmáticos_em_JAVA/main.java
@@ -0,0 +1,25 @@
+//VARIÁVEIS
+
+public class Main {
+
+    public static void main(string[] args) {
+
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a; //não é boa prática iniciar com _
+        int $aq; //não é boa prática iniciar com $
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;  j já
+        int asrn32154md;
+        // int askj354s 543;
+
+    }
+}
